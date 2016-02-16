e048b39cb8182bb3db892e4f9b90592b39abd690
RHeijblom <RHeijblom@users.noreply.github.com>
2016-02-16 10:27:11 +0100

    Adds the experiments + scripts for analysis
    - Adds two misc testruns
    - Adds three testruns of first experiment (gran =5,10,20)
    - Adds archive
    - Adds script failed_summarizer.sh to extract from failed.txt of a run
    - Adds excel overview

---
+++ Test/batch_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#steps 1 - 608
+args='-N1 -n1 --mem=8000 --time=35:00 --partition=r415'
+srun $args /home/s1083392/experiments/generated/steps/step_e896ba8a-0b3e-426c-8c59-0a1828072991.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_a4a697be-bf2d-4e87-bf7a-49cb576edfd4.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_270f04cb-7deb-4640-9489-de5a042d1a85.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_a5adcea4-9fd0-403b-9af2-2e3e88bcb7a0.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_4d5f78b3-b0b5-4284-9952-c7b476a912f3.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_a923557e-a3d7-4238-b33b-cd8c5490071b.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_37b00eed-e5f8-40f6-aca5-32b09f0c9dc7.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_eadd5c4c-b897-43df-bb2b-b13b9319f5eb.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_06cd94f5-02b1-4d14-bf7d-521394668cef.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_780d4824-a062-49c2-8a09-155c34b80687.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_4196f009-27eb-4ab7-9f16-0ee68915d92f.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_e2ab44c1-b4ef-4f39-8b75-551495e8e1c5.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_80a52540-9215-4cfe-949e-2e1b554d80a4.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_6e1a9a43-6894-4c6b-95d2-779376b00b4a.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_6fc9a8e5-0043-47f3-b3b5-40f85ad62858.sh &
+srun $args /home/s1083392/experiments/generated/steps/step_1a117474-7d67-4325-b446-c5c8eb266711.sh &
---
+++ failed_summarizer.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Model is written as <MODEL> = <PREFIX><NAME><SUFFIX>
+# PREFIX and SUFFIX are defined in order to extract <NAME> from <MODEL>
+PREFIX='/home/s1083392/experiments/in/ptAll/'
+SUFFIX='.pnml'
+
+INPUT_FILE=$1
+TMP_FILE="./tmp_modelnames.txt"
+
+if [[ -n $INPUT_FILE  && -f $INPUT_FILE ]]; then
+	# Initialize temporary file to store intermediate collections
+	if [[ -e $TMP_FILE ]]; then
+		rm $TMP_FILE
+	fi
+	touch $TMP_FILE
+	
+	# Extract names of all models in file
+	LEN_PRE=${#PREFIX}
+	LEN_SUF=${#SUFFIX}
+	awk '{
+		# Last colums/field contains path to model
+		if( substr($NF, 1, '"$LEN_PRE"') == "'"$PREFIX"'" && substr($NF, length($NF)+1-'"$LEN_SUF"', '"$LEN_SUF"') == "'"$SUFFIX"'")
+			# Print name without PREFIX and SUFFIX
+			print substr($NF,'"$LEN_PRE"'+1, length($NF)-'"$LEN_PRE"'-'"$LEN_SUF"')
+		else
+			# Model does not match; keep entire name
+			print $NF
+    }' "$INPUT_FILE" > "$TMP_FILE"
+	
+	# Sort and filter names to obtain unique set of names
+	MODEL_IDS=$(sort "$TMP_FILE" | uniq | xargs)
+	
+	# Find and count occurrences of each model
+	total=0
+	totalUnique=0
+	for name in $MODEL_IDS; do
+		count=$(grep -r $PREFIX$name$SUFFIX $INPUT_FILE | wc -l)
+		echo "$name: $count"
+		total=$(($total + $count)) 
+		totalUnique=$(($totalUnique + 1))
+	done
+	
+	# Echo summary
+	echo "Total number of models: $total"
+	echo "Total number of unique models: $totalUnique"
+else
+	# Error; no suitable input file is given
+	echo "No input file given to be analyzed"
+	echo "Usage: $0 <file>"
+fi
